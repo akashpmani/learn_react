391262f6436f7b5cec835bae95988fe784c84a5c
akashpmani <akashpmani@gmail.com>
2023-09-15 21:47:10 +0530

    darkmode

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import PropTypes from 'prop-types'
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title = "LEARN REACT"/>
-      <div className="container">
-      <TextForm/>
+      <div className="container my-5">
+      {/* <TextForm/> */}
+      <About/>
       </div>
     </>
   );
