12c81e595e0a9821152cf1ec4435ec31225e7b0d
akashpmani <akashpmani@gmail.com>
2023-09-15 11:57:52 +0530

    firs

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const a = <div> <h1>hhss</h1></div>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <div> <h1>hhss</h1></div>
+
 );
+// ReactDOM.render(
+//   "hiii",
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
