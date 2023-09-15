368a630f9304a540113df6ac88cd7b85646f2647
akashpmani <akashpmani@gmail.com>
2023-09-15 20:10:50 +0530

    text to speech

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,12 +2,28 @@ import React, { useState } from 'react'
 
 
 export default function TextForm() {
-  const handleConvert = () =>{
+  const handleConvertup = () =>{
     let newText = text.toUpperCase(text)
+    setText(newText)
+  }
+  const handleConvertlow = () =>{
+    let newText = text.toLowerCase(text)
     setText(newText)
   }
   const handleOnChange = (event) => {
     setText(event.target.value)
+  }
+  const handleClear = (event) => {
+    setText('')
+  }
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text)
+    alert("copied successfully")
+  }
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
   }
   const [text , setText] = useState(() => {
     return "enter text here"
@@ -18,8 +34,13 @@ export default function TextForm() {
   <label htmlFor="exampleFormControlTextarea1" className="form-label"></label>
   <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleOnChange} rows="10"></textarea>
   <br />
-  <button className="btn btn-primary" onClick={handleConvert}>convert</button>
-  
+  <div>  
+    <button className="btn btn-primary mx-3" onClick={handleConvertup}>Convert to upper</button>
+    <button className="btn btn-primary mx-3 " onClick={handleConvertlow}>Convert to lower</button>
+    <button className="btn btn-success mx-3 " onClick={handleCopy}>Copy</button>
+    <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+    <button className="btn btn-danger mx-3 " onClick={handleClear}>Clear</button>
+  </div>
 </div>
 <div className="container my-3">
 <h6>Your Text Summary</h6>
